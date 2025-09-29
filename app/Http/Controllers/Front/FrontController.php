270d4f8875885feec5ea20e09e6bbb91ac2f8f5b
Himel-032 <139494430+Himel-032@users.noreply.github.com>
2025-09-30 02:00:56 +0600

    Property will hide from frontend if the current package is expired

--- app/Http/Controllers/Front/FrontController.php
+++ app/Http/Controllers/Front/FrontController.php
@@ -16,13 +16,29 @@ class FrontController extends Controller
 {
     public function index()
     {
-        $properties = Property::where('status','Active')->orderBy('id','asc')->take(3)->get();
+        $properties = Property::where('status', 'Active')
+            ->whereHas('agent', function ($query) {
+                $query->whereHas('orders', function ($q) {
+                    $q->where('status', 'Completed')
+                        ->where('expire_date', '>=', now());
+                });
+            })
+            ->orderBy('id', 'asc')->take(3)->get();
         // get the total location wise property
-        $locations = Location::withCount(['properties' => function ($query) {
-            $query->where('status', 'Active');
-        }])->orderBy('name', 'asc')->orderBy('properties_count','desc')->take(4)->get();
+        $locations = Location::withCount([
+            'properties' => function ($query) {
+                $query->where('status', 'Active')
+                ->whereHas('agent', function($q) {
+                    $q->whereHas('orders', function ($qq) {
+                        $qq->where('currently_active', 1)
+                            ->where('status', 'Completed')
+                            ->where('expire_date', '>=', now());
+                    });
+                });
+            }
+        ])->orderBy('name', 'asc')->orderBy('properties_count', 'desc')->take(4)->get();
 
-        $agents = Agent::where('status',1)->orderBy('id','asc')->take(4)->get();
+        $agents = Agent::where('status', 1)->orderBy('id', 'asc')->take(4)->get();
 
         return view('front.home', compact('properties', 'locations', 'agents'));
     }
@@ -34,69 +50,110 @@ class FrontController extends Controller
     {
         return view('front.select_user');
     }
-    public function pricing(){
-        $packages = Package::orderBy('id','asc')->get();
+    public function pricing()
+    {
+        $packages = Package::orderBy('id', 'asc')->get();
         return view('front.pricing', compact('packages'));
     }
 
-    public function property_detail($slug){
-        $property = Property::where('slug',$slug)->first();
-        if(!$property){
-            return redirect()->route('front.home')->with('error','Property not found');
+    public function property_detail($slug)
+    {
+        $property = Property::where('slug', $slug)->first();
+        if (!$property) {
+            return redirect()->route('front.home')->with('error', 'Property not found');
         }
-        
+
         return view('front.layouts.property_detail', compact('property'));
     }
-    public function property_send_message(Request $request, $id){
+    public function property_send_message(Request $request, $id)
+    {
         $property = Property::find($id);
-        if(!$property){
-            return redirect()->route('home')->with('error','Property not found');
+        if (!$property) {
+            return redirect()->route('home')->with('error', 'Property not found');
         }
         //send email to agent
         $subject = "Property Enquiry";
-        $message = "You have received an enquiry for the property: ".$property->name. '<br><br>';
-        $message .= 'Visitor Name: '.$request->name.'<br>';
-        $message .= 'Visitor Email: '.$request->email.'<br>';
-        $message .= 'Visitor Phone: '.$request->phone.'<br>';
-        $message .= 'Visitor Message: '.nl2br($request->message);
+        $message = "You have received an enquiry for the property: " . $property->name . '<br><br>';
+        $message .= 'Visitor Name: ' . $request->name . '<br>';
+        $message .= 'Visitor Email: ' . $request->email . '<br>';
+        $message .= 'Visitor Phone: ' . $request->phone . '<br>';
+        $message .= 'Visitor Message: ' . nl2br($request->message);
         $agent_email = $property->agent->email;
         \Mail::to($agent_email)->send(new Websitemail($subject, $message));
-        return redirect()->back()->with('success','Your message has been sent successfully. Agent will contact you soon.');
+        return redirect()->back()->with('success', 'Your message has been sent successfully. Agent will contact you soon.');
     }
 
     public function locations()
     {
-        // get total property in a particular location
-        $locations = Location::withCount(['properties' => function ($query) {
-            $query->where('status', 'Active');
-        }])->orderBy('properties_count','desc')->paginate(20);
+       // get total property in a particular location
+        // $locations = Location::withCount([
+        //     'properties' => function ($query) {
+        //         $query->where('status', 'Active');
+        //     }
+        // ])->orderBy('properties_count', 'desc')->paginate(20);
+
+        // Hide property whose package is expired
+        $locations = Location::withCount([
+            'properties' => function ($query) {
+                $query->where('status', 'Active')
+                    ->whereHas('agent', function ($q) {
+                        $q->whereHas('orders', function ($qq) {
+                            $qq->where('currently_active', 1)
+                                ->where('status', 'Completed')
+                                ->where('expire_date', '>=', now());
+                        });
+                    });
+            }
+        ])->orderBy('name', 'asc')->orderBy('properties_count', 'desc')->paginate(20);
         return view('front.locations', compact('locations'));
 
     }
 
     public function location($slug)
     {
-        $location = Location::where('slug',$slug)->first();
-        if(!$location){
-            return redirect()->route('front.locations')->with('error','Location not found');
+        $location = Location::where('slug', $slug)->first();
+        if (!$location) {
+            return redirect()->route('front.locations')->with('error', 'Location not found');
         }
-        $properties = Property::where('location_id',$location->id)->where('status','Active')->orderBy('id','asc')->paginate(6);
-        return view('front.location', compact('location','properties'));
+        //  $properties = Property::where('location_id',$location->id)->where('status','Active')->orderBy('id','asc')->paginate(6);
+
+
+        $properties = Property::where('location_id', $location->id)
+            ->where('status', 'Active')
+            ->whereHas('agent', function ($query) {
+                $query->whereHas('orders', function ($q) {
+                    $q->where('status', 'Completed')
+                        ->where('expire_date', '>=', now());
+                });
+            })
+            ->orderBy('id', 'asc')->take(3)->paginate(6);
+
+        return view('front.location', compact('location', 'properties'));
     }
 
     public function agents()
     {
-        $agents = Agent::where('status',1)->orderBy('id','asc')->paginate(20);
+        $agents = Agent::where('status', 1)->orderBy('id', 'asc')->paginate(20);
         return view('front.agents', compact('agents'));
     }
     public function agent($id)
     {
         $agent = Agent::where('id', $id)->first();
-        if(!$agent){
-            return redirect()->route('home')->with('error','Agenttt not found');
+        if (!$agent) {
+            return redirect()->route('home')->with('error', 'Agenttt not found');
         }
-        $properties = Property::where('agent_id',$agent->id)->where('status','Active')->orderBy('id','asc')->paginate(6);
-        return view('front.agent', compact('agent','properties'));
+        // $properties = Property::where('agent_id',$agent->id)->where('status','Active')->orderBy('id','asc')->paginate(6);
+
+        $properties = Property::where('agent_id', $agent->id)
+            ->where('status', 'Active')
+            ->whereHas('agent', function ($query) {
+                $query->whereHas('orders', function ($q) {
+                    $q->where('status', 'Completed')
+                        ->where('expire_date', '>=', now());
+                });
+            })
+            ->orderBy('id', 'asc')->take(3)->paginate(6);
+        return view('front.agent', compact('agent', 'properties'));
     }
 
 }
